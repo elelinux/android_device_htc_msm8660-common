c7292934c99d697de3aec620d94b084546e1cd8f
elelinux <ele.linux@gmail.com>
2013-04-26 19:20:10 +0200

    rename liblights

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -93,7 +93,7 @@ TARGET_USES_PMEM := true
 BOARD_EGL_CFG := device/htc/msm8660-common/configs/egl.cfg
 
 # Lights
-TARGET_PROVIDES_LIBLIGHTS := true
+TARGET_PROVIDES_LIBLIGHT := true
 
 # Filesystem
 BOARD_VOLD_MAX_PARTITIONS := 36
