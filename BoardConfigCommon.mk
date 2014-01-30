c1c3608435b15a28422e9a88a87b8dcc664cbf3a
elelinux <ele.linux@gmail.com>
2014-01-30 22:39:14 +0100

    fix typo

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -87,7 +87,7 @@ USE_OPENGL_RENDERER := true
 TARGET_DISPLAY_USE_RETIRE_FENCE := true
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
-BOARD_EGL_CFG := device/htc/msm-8660/configs/egl.cfg
+BOARD_EGL_CFG := device/htc/msm8660-common/configs/egl.cfg
 TARGET_DISPLAY_INSECURE_MM_HEAP := true
 
 # Lights
